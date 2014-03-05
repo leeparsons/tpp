ccb5fe1ecb8ac183c034f162679a5f026bbda687
Lee Parsons <lee@organic-development.com>
2014-03-05 17:45:21 +0000

    deleted old files

--- wp-content/plugins/tppstore/site/views/account/sidebar.php
+++
@@ -1,8 +0,0 @@
-<aside class="aside-25">
-    <nav>
-        <ul>
-            <li><a href="/shop/store_logout">Logout</a></li>
-            <li><a href="/shop/myaccount/details">My Details</a></li>
-        </ul>
-    </nav>
-</aside>
